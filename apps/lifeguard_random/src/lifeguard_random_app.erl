92b985bd58e6b0ac38f55c897b738f195257b783
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-06-06 16:02:44 -0700

    Initial commit of app structure

---
+++ apps/lifeguard_random/src/lifeguard_random_app.erl
@@ -0,0 +1,17 @@
+-module(lifeguard_random_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    io:format("App started.~n"),
+    lifeguard_random_sup:start_link().
+
+stop(_State) ->
+    ok.
